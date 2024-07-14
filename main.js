a4a7e4cae711f8b0a179bd757053b5d70e755ed7
martijn001 <164541002+martijn001@users.noreply.github.com>
2024-07-14 20:15:05 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,1 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
 
-function createWindow() {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'renderer.js'),
-            nodeIntegration: true
-        }
-    });
-
-    mainWindow.loadFile('index.html');
-}
-
-app.whenReady().then(createWindow);
