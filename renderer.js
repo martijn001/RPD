8328d873e0149745565888c7c35daf45dd64834b
martijn001 <164541002+martijn001@users.noreply.github.com>
2024-07-14 20:15:19 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,13 +1,1 @@
-const { ipcRenderer } = require('electron');
 
-const editor = document.getElementById('editor');
-
-// Example: Listen for messages from main process
-ipcRenderer.on('message-from-main', (event, data) => {
-    console.log('Received message from main process:', data);
-});
-
-// Example: Send message to main process
-function sendMessageToMain() {
-    ipcRenderer.send('message-to-main', 'Hello from renderer process!');
-}
